41e8df0a84daafb6054a8d3f2dd1f1be236c8d2e
Fangzhou Lin <adamlangwang@gmail.com>
2020-08-03 15:23:56 +0800

    Fix bug

--- Users/access_log_user.py
+++ Users/access_log_user.py
@@ -1,13 +1,10 @@
-from Utils.singleton import get_access_log
 from locust.contrib.fasthttp import FastHttpUser
 from Utils.parser import parse
+from Utils.util import get_slash
+import os
 
 
 class AccessLogUser(FastHttpUser):
     min_wait = 100
     max_wait = 120
-
-    def __init__(self, environment):
-        super().__init__(environment)
-        access_log = get_access_log()
-        self.tasks = parse(access_log)
+    tasks = parse(os.path.abspath(f'.{get_slash()}access.log'))
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import argparse
 import os
 import logging
 import threading
-from Utils.singleton import set_access_log
 from Utils.util import get_slash
 from Users.access_log_user import AccessLogUser
 from locust_runner import LocustRunner
@@ -15,8 +14,7 @@ def config_argument_parser():
     parser.add_argument('--target_server_address', help='Http server to send the request', required=True)
     parser.add_argument('--process_pid', help='Target process to monitor on')
     parser.add_argument('--process_name', help='Target process to monitor on')
-    parser.add_argument('--collect_time_in_second', help='Total time to run', default=60, type=int)
-    parser.add_argument('--access_log', help='access log to config locust', default=get_default_access_log())
+    parser.add_argument('--collect_time_in_second', help='Total time to run', default=60, type=float)
     parser.add_argument('--locust_csv_prefix', help='locust csv prefix')
     parser.add_argument('--total_user', help='Amount user to simulate', default=100, type=int)
     parser.add_argument('--hatch_rate', help='User hatch rate', default=10, type=int)
@@ -41,13 +39,9 @@ def get_default_locust_csv_prefix(process_name_or_pid):
 
 def get_default_process_monitor_csv_prefix(name, pid):
     if name is None:
-        return f'{os.path.dirname(os.path.realpath(__file__))}{get_slash()}{name}_process.csv'
-    else:
         return f'{os.path.dirname(os.path.realpath(__file__))}{get_slash()}{pid}_process.csv'
-
-
-def get_default_access_log():
-    return f'{os.path.dirname(os.path.realpath(__file__))}{get_slash()}access.log'
+    else:
+        return f'{os.path.dirname(os.path.realpath(__file__))}{get_slash()}{name}_process.csv'
 
 
 def run_locust(locust_runner: LocustRunner, options):
@@ -75,7 +69,6 @@ if __name__ == '__main__':
     setup_logging('INFO')
     args = config_argument_parser()
     logger = logging.getLogger(__name__)
-    set_access_log(str(args.access_log))
     locust_runner = LocustRunner(args.target_server_address, AccessLogUser)
     try:
         process_monitor = ProcessMonitor(pid=args.process_pid,
@@ -85,10 +78,11 @@ if __name__ == '__main__':
     except:
         logger.warning('Will not run process monitor')
         pass
-    locust_task = threading.Thread(run_locust, args=(locust_runner, args))
-    process_monitor_task = threading.Thread(run_process_monitor, args=(process_monitor, args))
-    locust_task.run()
-    process_monitor_task.run()
+    locust_task = threading.Thread(target=run_locust, args=(locust_runner, args))
+    process_monitor_task = threading.Thread(target=run_process_monitor, args=(process_monitor, args))
+    locust_task.start()
+    process_monitor_task.start()
     locust_task.join()
-    process_monitor_task.run()
+    process_monitor_task.join()
+    logger.info("Load test done")
 
